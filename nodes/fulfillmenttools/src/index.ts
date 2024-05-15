95ecb29321c332326d1e8863b8f37f82d817cfcd
Marco Spengler <marco.spengler@skriptfabrik.com>
2024-05-15 11:01:52 +0200

    N8N-2 Remove unused barrel files including unused typescript paths configuration

--- nodes/fulfillmenttools/src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './credentials/FulfillmenttoolsApi.credentials';
-export * from './nodes/Fulfillmenttools/Fulfillmenttools.node';
-export * from './nodes/Fulfillmenttools/FulfillmenttoolsTrigger.node';
--- nodes/kaufland-marketplace/src/index.ts
+++
@@ -1,1 +0,0 @@
-export * from './nodes/KauflandMarketplace/KauflandMarketplace.node';
