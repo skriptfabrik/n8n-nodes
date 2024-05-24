78bb0b4e16663df55c3ee8f6629153d92fe17590
Marco Spengler <marco.spengler@skriptfabrik.com>
2024-05-24 08:18:33 +0200

    N8N-2 Replace non existing load options name for retrieving Clockify workspace projects

--- nodes/clockify-enhanced/src/nodes/ClockifyEnhanced/descriptions/ProjectDescription.ts
+++ nodes/clockify-enhanced/src/nodes/ClockifyEnhanced/descriptions/ProjectDescription.ts
@@ -35,7 +35,7 @@ export const projectFields: INodeProperties[] = [
       'Choose from the list, or specify an ID using an <a href="https://docs.n8n.io/code-examples/expressions/">expression</a>',
     typeOptions: {
       loadOptionsDependsOn: ['workspaceId'],
-      loadOptionsMethod: 'loadProjectsForWorkspace',
+      loadOptionsMethod: 'loadProjects',
     },
     required: true,
     default: '',
