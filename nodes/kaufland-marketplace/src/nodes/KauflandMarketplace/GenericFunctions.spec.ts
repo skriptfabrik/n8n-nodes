3ec5ac05b2cf16e8260dbeab1e00bac40fd0aa1c
xtrcode <43306099+xtrcode@users.noreply.github.com>
2025-07-21 11:12:41 +0200

    fix(kaufland-marketplace) test coverage (#N8N-31)

--- nodes/kaufland-marketplace/src/nodes/KauflandMarketplace/GenericFunctions.spec.ts
+++ nodes/kaufland-marketplace/src/nodes/KauflandMarketplace/GenericFunctions.spec.ts
@@ -19,11 +19,9 @@ describe('Kaufland Marketplace Request', () => {
         clientSecret: 'testClientSecret',
       }),
       helpers: {
-        requestWithAuthentication: jest
-          .fn()
-          .mockResolvedValueOnce({
-            data: ['item 1', 'item 2'],
-          }),
+        requestWithAuthentication: jest.fn().mockResolvedValueOnce({
+          data: ['item 1', 'item 2'],
+        }),
       },
     });
 
@@ -138,5 +136,63 @@ describe('Kaufland Marketplace Request', () => {
     await expect(
       kauflandMarketplaceRequest(instance, requestData, 1),
     ).rejects.toThrow();
+  });
+
+  it('should fetch all pages when pagination indicates more data', async () => {
+    const firstPage = {
+      data: [{ foo: 1 }],
+      pagination: { offset: 0, limit: 1, total: 2 },
+    };
+    const secondPage = {
+      data: [{ foo: 2 }],
+      pagination: { offset: 1, limit: 1, total: 2 },
+    };
+
+    const instance = mockDeep<IExecuteFunctions>({
+      getCredentials: jest.fn().mockResolvedValue({
+        clientId: 'testClientId',
+        clientSecret: 'testClientSecret',
+      }),
+      helpers: {
+        requestWithAuthentication: jest
+          .fn()
+          .mockResolvedValueOnce(firstPage)
+          .mockResolvedValueOnce(secondPage),
+      },
+    });
+
+    const requestData: KauflandRequestData = {
+      method: 'GET',
+      uri: `/v2/test`,
+      body: '',
+      // no qs so it uses default
+    };
+
+    const expected = [...firstPage.data, ...secondPage.data];
+    const result = await kauflandMarketplaceRequest(instance, requestData);
+
+    expect(result.data).toStrictEqual(expected);
+  });
+
+  it('handles an undefined response safely', async () => {
+    const instance = mockDeep<IExecuteFunctions>({
+      getCredentials: jest.fn().mockResolvedValue({
+        clientId: 'testClientId',
+        clientSecret: 'testClientSecret',
+      }),
+
+      helpers: {
+        requestWithAuthentication: jest
+          .fn()
+          .mockResolvedValueOnce(undefined as any),
+      },
+    });
+    const result = await kauflandMarketplaceRequest(instance, {
+      method: 'GET',
+      uri: '/v2/foo',
+      body: '',
+    });
+
+    expect(result).toEqual(undefined);
   });
 });
